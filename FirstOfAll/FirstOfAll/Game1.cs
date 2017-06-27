1b4f1269612d8450d386ba81afb52cc3465fcb04
SaintForge <mr.spartan-117@yandex.ru>
2017-06-27 19:13:06 +0300

    added comment manamejeeff

--- FirstOfAll/FirstOfAll/Game1.cs
+++ FirstOfAll/FirstOfAll/Game1.cs
@@ -16,6 +16,7 @@ namespace FirstOfAll
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            // MANAMEJEEEEEEEEEEEEEEEEF
         }
 
         /// <summary>
